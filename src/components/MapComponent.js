970b39bf523f6aea6e7b92d9b206ef4cd3bd724e
Gabriel Nogueira <gabrielgoncalvesnogueira@gmail.com>
2019-04-02 15:10:22 -0300

    Mapa e Barra de Pesquisa adicionados

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,10 +1,83 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import { compose, withProps, lifecycle } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 
-function MapComponent() {
+import { StandaloneSearchBox } from "react-google-maps/lib/components/places/StandaloneSearchBox";
 
-    return (
-        <div></div>
-    )
-}
+const PlacesWithStandaloneSearch = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDYE-toUOpOxTafX7-agwEuRndBwE5valw&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  lifecycle({
+    componentWillMount() {
+      const refs = {};
 
-export default MapComponent
+      this.setState({
+        places: [],
+        onSearchBoxMounted: ref => {
+          refs.searchBox = ref;
+        },
+        onPlacesChanged: () => {
+          const places = refs.searchBox.getPlaces();
+
+          this.setState({
+            places
+          });
+        }
+      });
+    }
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <div data-standalone-searchbox="">
+    <StandaloneSearchBox
+      ref={props.onSearchBoxMounted}
+      bounds={props.bounds}
+      onPlacesChanged={props.onPlacesChanged}
+    >
+      <input
+        type="text"
+        placeholder="Customized your placeholder"
+        style={{
+          boxSizing: `border-box`,
+          border: `1px solid transparent`,
+          width: `240px`,
+          height: `32px`,
+          padding: `0 12px`,
+          borderRadius: `3px`,
+          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+          fontSize: `14px`,
+          outline: `none`,
+          textOverflow: `ellipses`
+        }}
+      />
+    </StandaloneSearchBox>
+    <ol>
+      {props.places.map(
+        ({ place_id, formatted_address, geometry: { location } }) => (
+          <li key={place_id}>
+            {formatted_address}
+            {" at "}({location.lat()}, {location.lng()})
+          </li>
+        )
+      )}
+    </ol>
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }} />
+  </div>
+));
+
+ReactDOM.render(
+  <PlacesWithStandaloneSearch />,
+  document.getElementById("root")
+);
